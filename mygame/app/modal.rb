f5f54209e0d762d8cabd6924e9b3d0a397b277a3
denvermullets <devmullets@gmail.com>
2024-10-14 10:54:26 -0400

    chore(menu): add sprites and update colors

--- mygame/app/modal.rb
+++ mygame/app/modal.rb
@@ -1,8 +1,11 @@
 class Modal
   def initialize(_args)
-    @toggle_modal_button = { x: 10, y: 10, w: 300, h: 50, text: 'Open Menu' }
-    @purchase_button = { x: 20, y: 110, w: 200, h: 40, text: 'Purchase Animal' }
-    @remove_button = { x: 230, y: 110, w: 200, h: 40, text: 'Remove Animals' }
+    @menu_bg = { x: 40, y: 116, w: 200, h: 260, sprite: 'assets/bg-menu.png' }
+    @toggle_modal_button = { x: 40, y: 40, w: 200, h: 60, sprite: 'assets/btn-menu.png' }
+    @pause_button = { x: 60, y: 296, w: 160, h: 60, sprite: 'assets/btn-pause.png' }
+    @purchase_button = { x: 60, y: 216, w: 160, h: 60, sprite: 'assets/btn-buy-animal.png' }
+    @remove_button = { x: 60, y: 136, w: 160, h: 60, sprite: 'assets/btn-sell-animal.png' }
+    @stats_background = { x: 40, y: 427, w: 342, h: 253, sprite: 'assets/bg-animal-list.png' }
     @modal_open = false
   end
 
@@ -21,19 +24,20 @@ class Modal
 
   def render(args)
     player = args.state.player
-    args.outputs.labels << [10, 680, "Money: #{player.money}"]
-    args.outputs.labels << [10, 650, "Game Time: #{player.game_time}"]
-    args.outputs.labels << [10, 620, "Animals: #{player.animals.count}"]
-    args.outputs.labels << [10, 590, "Products: #{player.product_output}"]
-
-    render_button(args, @toggle_modal_button, 29, 234, 207)
+    text_color = [251, 251, 251]
+    render_sprite(args, @stats_background)
+    args.outputs.labels << [70, 660, "Money: #{player.money}", text_color]
+    args.outputs.labels << [70, 630, "Game Time: #{player.game_time}", text_color]
+    args.outputs.labels << [70, 600, "Animals: #{player.animals.count}", text_color]
+    args.outputs.labels << [70, 570, "Products: #{player.product_output}", text_color]
+    render_sprite(args, @toggle_modal_button)
 
     return unless @modal_open
 
-    # background for the modal
-    args.outputs.solids << [10, 100, 430, 100, 52, 55, 57]
-    render_button(args, @purchase_button, 233, 29, 71)
-    render_button(args, @remove_button, 29, 162, 234)
+    render_sprite(args, @menu_bg)
+    render_sprite(args, @purchase_button)
+    render_sprite(args, @pause_button)
+    render_sprite(args, @remove_button)
   end
 
   private
@@ -42,8 +46,7 @@ class Modal
     args.inputs.mouse.point.inside_rect? button
   end
 
-  def render_button(args, button, r, g, b)
-    args.outputs.solids << [button.values_at(:x, :y, :w, :h), r, g, b]
-    args.outputs.labels << [button[:x] + 10, button[:y] + 30, button[:text]]
+  def render_sprite(args, sprite)
+    args.outputs.sprites << [sprite[:x], sprite[:y], sprite[:w], sprite[:h], sprite[:sprite]]
   end
 end
